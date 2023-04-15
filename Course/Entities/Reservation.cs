7d1c5cb127f484984f7e93dc4fe172d3d97b0e1f
Marcelo Carneiro <marcelo.dev100@gmail.com>
2023-04-14 22:03:55 -0300

    very bad solution: validation logic in the Main class

---
+++ Course/Entities/Reservation.cs
@@ -0,0 +1,52 @@
+using System.Text;
+
+namespace Course.Entities
+{
+    public class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return duration.Days;
+           //TimeSpan duration = CheckOut.Subtract(CheckIn);
+           //return (int) duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkIn , DateTime checkOut)
+        {
+             CheckIn = checkIn;
+             CheckOut = checkOut;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Room ");
+            sb.Append(RoomNumber);
+            sb.Append(", check-in: ");
+            sb.Append(CheckIn.ToString("dd/MM/yyyy"));
+            sb.Append(", check-out: ");
+            sb.Append(CheckOut.ToString("dd/MM/yyyy"));
+            sb.Append(", ");
+            sb.Append(Duration().ToString());
+            sb.AppendLine(" nights");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+C# .NET 6.0 (VsCode)
+
+Write a program to read data from a hotel reservation(room number, check-in and check-out dates) and display reservation data, including its duration in days. Then, read new check-in and check-out dates and display again reservation updated data. The program must not accept invalid data for the reservation, as the following rules:
+
+
+* Reservation updates only can occur at future dates.
+* Check-out date must be higher than check-in date.
